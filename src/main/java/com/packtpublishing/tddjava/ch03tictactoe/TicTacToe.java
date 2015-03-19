c7276f891662cd40da0500452ff5e27ae25b5eb0
Viktor Farcic <viktor@farcic.com>
2015-03-19 22:17:18 +0100

    Exceptions

--- src/main/java/com/packtpublishing/tddjava/ch03tictactoe/TicTacToe.java
+++ src/main/java/com/packtpublishing/tddjava/ch03tictactoe/TicTacToe.java
@@ -1,7 +1,20 @@
 package com.packtpublishing.tddjava.ch03tictactoe;
 
-/**
- * Created by vfarcic on 19/03/15.
- */
 public class TicTacToe {
+
+    private Character[][] board = {{null, null, null}, {null, null, null}, {null, null, null}};
+
+    public void play(int x, int y) {
+        if (x < 1 || x > 3) {
+            throw new RuntimeException("X is outside board");
+        } else if (y < 1 || y > 3) {
+            throw new RuntimeException("Y is outside board");
+        }
+        if (board[x - 1][y - 1] != null) {
+            throw new RuntimeException("Y is outside board");
+        } else {
+            board[x - 1][y - 1] = 'X';
+        }
+    }
+
 }
--- src/test/java/com/packtpublishing/tddjava/ch03tictactoe/TicTacToeSpec.java
+++ src/test/java/com/packtpublishing/tddjava/ch03tictactoe/TicTacToeSpec.java
@@ -1,4 +1,39 @@
 package com.packtpublishing.tddjava.ch03tictactoe;
 
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
 public class TicTacToeSpec {
+
+    @Rule
+    public ExpectedException exception = ExpectedException.none();
+    private TicTacToe ticTacToe;
+
+    @Before
+    public final void before() {
+        ticTacToe = new TicTacToe();
+    }
+
+    @Test
+    public void whenXOutsideBoardThenRuntimeException() {
+        exception.expect(RuntimeException.class);
+        ticTacToe.play(5, 2);
+    }
+
+    @Test
+    public void whenYOutsideBoardThenRuntimeException() {
+        exception.expect(RuntimeException.class);
+        ticTacToe.play(2, 5);
+    }
+
+    @Test
+    public void whenOccupiedThenRuntimeException() {
+        ticTacToe.play(1, 1);
+        ticTacToe.play(2, 1);
+        exception.expect(RuntimeException.class);
+        ticTacToe.play(2, 1);
+    }
+
 }
