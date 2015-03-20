07ab64d81e7613e3591ca4d71ead48ba84a3af8a
Viktor Farcic <viktor@farcic.com>
2015-03-21 00:49:56 +0100

    Refactoring

--- src/main/java/com/packtpublishing/tddjava/ch03tictactoe/TicTacToe.java
+++ src/main/java/com/packtpublishing/tddjava/ch03tictactoe/TicTacToe.java
@@ -11,7 +11,7 @@ public class TicTacToe {
         checkAxis(y);
         lastPlayer = nextPlayer();
         setBox(x, y, lastPlayer);
-        if (isWin()) {
+        if (isWin(x, y)) {
             return lastPlayer + " is the winner";
         } else if (isDraw()) {
             return "The result is draw";
@@ -41,20 +41,20 @@ public class TicTacToe {
         }
     }
 
-    private boolean isWin() {
+    private boolean isWin(int x, int y) {
         int playerTotal = lastPlayer * 3;
-        char diagonal1 = '\0';
-        char diagonal2 = '\0';
+        char horizontal, vertical, diagonal1, diagonal2;
+        horizontal = vertical = diagonal1 = diagonal2 = '\0';
         for (int i = 0; i < SIZE; i++) {
+            horizontal += board[i][y - 1];
+            vertical += board[x - 1][i];
             diagonal1 += board[i][i];
             diagonal2 += board[i][SIZE - i - 1];
-            if ((board[0][i] + board[1][i] + board[2][i]) == playerTotal) {
-                return true;
-            } else if ((board[i][0] + board[i][1] + board[i][2]) == playerTotal) {
-                return true;
-            }
         }
-        if (diagonal1 == playerTotal || diagonal2 == playerTotal) {
+        if (horizontal == playerTotal
+                || vertical == playerTotal
+                || diagonal1 == playerTotal
+                || diagonal2 == playerTotal) {
             return true;
         }
         return false;
